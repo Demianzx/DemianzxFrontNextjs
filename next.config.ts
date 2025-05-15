9d1fcccc478ce459fa1ade34ada60e5b3b0c2e7a
Demainzx <demianz999@gmail.com>
2025-05-14 21:08:32 -0600

    added port .env

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
     domains: ['demianzxgamesstorage.blob.core.windows.net','picsum.photos'],
   },  
   output: 'standalone',
+  serverRuntimeConfig: {
+    port: process.env.PORT || 3000,
+  },
+  env: {
+    PORT: process.env.PORT || '3000'
+  }
 };
 
 export default nextConfig;
